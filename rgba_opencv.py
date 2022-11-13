591e54977607cfbc83d0abc4340e6ce97c3df33b
watajun <wtajun6@yahoo.co.jp>
2022-11-13 11:06:27 +0900

    add

---
+++ rgba_opencv.py
@@ -0,0 +1,5 @@
+import cv2
+
+mask_img = cv2.imread("images/otahuku.png")
+print(mask_img)
+print(mask_img.sharw)
--- trans_app.py
+++ trans_app.py
@@ -1,4 +1,5 @@
 import cv2
+from PIL import Image 
 
 original_face_img = "images/man.png"
 original_mask_img = "images/otahuku.png"
@@ -18,12 +19,19 @@ def paste_img(face_list):
     y = face_list[0][1]
     w = face_list[0][2]
     h = face_list[0][3]
-    face_img = cv2.imread(original_face_img)
-    mask_img = cv2.imread(original_mask_img)
-    resize_mask_img = cv2.resize(mask_img, dsize=(w, h))
-    cv2.imwrite("images/resize_otahuku.png", resize_mask_img)
-    face_img[y : h + y, x : x + w] = cv2.imread("images/resize_otahuku.png")
-    cv2.imwrite("images/hensin.png", face_img)
+    # コメントアウトがopencvで実行するコード
+    # face_img = cv2.imread(original_face_img)
+    # mask_img = cv2.imread(original_mask_img)
+    face_img = Image.open(original_face_img)
+    mask_img = Image.open(original_mask_img)
+    # resize_mask_img = cv2.resize(mask_img, dsize=(w, h))
+    resize_mask_img = mask_img.resize((w, h))
+    # cv2.imwrite("images/resize_otahuku.png", resize_mask_img)
+    resize_mask_img.save("images/resize_otahuku_pillow.png")
+    # face_img[y : h + y, x : x + w] = cv2.imread("images/resize_otahuku.png")
+    # cv2.imwrite("images/hensin.png", face_img)
+    face_img.paste(resize_mask_img, (x, y), resize_mask_img.split()[3])
+    face_img.save("images/hensin_pillow.png")
 
 
 face_list = find_face()
